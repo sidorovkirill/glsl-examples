9db55f18f2f7477eb76bc50db63f2b932f0c2229
sidorovk <sidorovk@doxare.com>
2020-11-10 06:20:21 +0300

    Blending colors

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -5,8 +5,9 @@ uniform float u_time;
 
 
 void main() {
-//    vec2 v = u_mouse / u_resolution;
-//    vec3 color = vec3(v.x, v.y, 1.0);
-    vec3 color = vec3((sin(u_time) + 1.0) / 2.0, 0.0, (cos(u_time) + 1.0) / 2.0);
+    vec3 startColor = vec3(0.0, 0.0, 1.0);
+    vec3 endColor = vec3(1.0, 0.0, 0.0);
+    vec2 uv = gl_FragCoord.xy / u_resolution;
+    vec3 color = mix(startColor, endColor, uv.y);
     gl_FragColor = vec4(color, 1.0);
 }
