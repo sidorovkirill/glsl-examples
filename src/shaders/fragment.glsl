a89d37a76a316f176e096d23145339f1d422ca1f
sidorovk <sidorovk@doxare.com>
2020-11-10 06:39:01 +0300

    Varying

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -3,13 +3,11 @@ uniform vec2 u_mouse;
 uniform vec2 u_resolution;
 uniform float u_time;
 
+varying vec2 v_uv;
+varying vec3 v_position;
 
 void main() {
-    vec3 startColor = vec3(0.0, 0.0, 1.0);
-    vec3 endColor = vec3(1.0, 0.0, 0.0);
-    vec2 uv = gl_FragCoord.xy / u_resolution;
-    vec3 colorY = mix(startColor, endColor, uv.y);
-    vec3 colorX = mix(startColor, endColor, uv.x);
-    vec3 color = colorY * colorX;
+    // vec3 color = vec3(v_uv.x, v_uv.y, 1.0);
+    vec3 color = vec3(v_position.x, v_position.y, 1.0);
     gl_FragColor = vec4(color, 1.0);
 }
