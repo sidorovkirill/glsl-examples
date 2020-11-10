f6d45b29587094a69fe2f11a2ca05d15b309a721
sidorovk <sidorovk@doxare.com>
2020-11-10 04:31:33 +0300

    Use time parameter

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,10 +1,12 @@
 uniform vec3 u_color;
 uniform vec2 u_mouse;
 uniform vec2 u_resolution;
+uniform float u_time;
 
 
 void main() {
-    vec2 v = u_mouse / u_resolution;
-    vec3 color = vec3(v.x, v.y, 1.0);
+//    vec2 v = u_mouse / u_resolution;
+//    vec3 color = vec3(v.x, v.y, 1.0);
+    vec3 color = vec3((sin(u_time) + 1.0) / 2.0, 0.0, (cos(u_time) + 1.0) / 2.0);
     gl_FragColor = vec4(color, 1.0);
 }
