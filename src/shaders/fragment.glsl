f166334389241daddc0b0da284bb33cd8f766908
sidorovk <sidorovk@doxare.com>
2020-11-10 06:24:50 +0300

    Blending colors by to axis

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -8,6 +8,8 @@ void main() {
     vec3 startColor = vec3(0.0, 0.0, 1.0);
     vec3 endColor = vec3(1.0, 0.0, 0.0);
     vec2 uv = gl_FragCoord.xy / u_resolution;
-    vec3 color = mix(startColor, endColor, uv.y);
+    vec3 colorY = mix(startColor, endColor, uv.y);
+    vec3 colorX = mix(startColor, endColor, uv.x);
+    vec3 color = colorY * colorX;
     gl_FragColor = vec4(color, 1.0);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,3 +1,3 @@
 void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position * 0.3, 1.0);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position * 0.5, 1.0);
 }
