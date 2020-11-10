b37ef4249027dde123e844fc735d32c7f88a1068
sidorovk <sidorovk@doxare.com>
2020-11-10 07:02:55 +0300

    Clamp, step, smoothstep

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -7,7 +7,12 @@ varying vec2 v_uv;
 varying vec3 v_position;
 
 void main() {
-    // vec3 color = vec3(v_uv.x, v_uv.y, 1.0);
-    vec3 color = vec3(v_position.x, v_position.y, 1.0);
+    vec3 color = vec3(1.0);
+    // color.r = clamp(v_position.x, 0.0, 1.0);
+    // color.g = clamp(v_position.y, 0.0, 1.0);
+    // color.r = step(-0.3, v_position.x);
+    // color.g = step(-0.3, v_position.y);
+    color.r = smoothstep(0.0, 0.3, v_position.x);
+    color.g = smoothstep(0.0, 0.3, v_position.y);
     gl_FragColor = vec4(color, 1.0);
 }
